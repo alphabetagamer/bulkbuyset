4cb9f04d0c48dfe18b62d166da2e1794b37989ed
alphabeta_g <34796341+alphabetagamer@users.noreply.github.com>
2018-07-28 10:56:17 +0530

    Update Bulk Set Buyer.user.js

--- Bulk Set Buyer.user.js
+++ Bulk Set Buyer.user.js
@@ -6,15 +6,22 @@
 // @author       Alphabeta_g
 // @website
 // @match        *://steamcommunity.com/*/gamecards/*
-// @require      https://code.jquery.com/jquery-3.1.1.min.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @grant        GM_addStyle
 // ==/UserScript==
 
 
+
 $(".gamecards_inventorylink").append("<button class='btn_grey_grey btn_medium' style='padding:5px 20px;float:left;margin: 5px 10px' id='Buy'>Mass BUY</button>");
-$('#Buy').click(function(){
-var cards=[];
+var gid2=window.location.pathname.split("/");
+    var gid=gid2[4];
     var i=0;
+    var c;
+var cards=[];
+
+$('#Buy').click(function(){
+
+
     $(".badge_detail_tasks .badge_card_set_text").each(function(){
        cards[i]=$(this).text().replace(/\s+/g, " ");
         i++;
@@ -37,19 +44,40 @@ var cards=[];
 
         }
         cards[i]=s.trim().replace(/ /g,"%20");
+       var js="https://steamcommunity.com/market/priceoverview/?country=IN&currency=24&appid=753&market_hash_name="+gid+"-"+cards[i]+"&json=true";
+       c = function () {
+    var tmp = 0;
+    $.ajax({
+        'async': false,
+        'type': "POST",
+        'global': false,
+        'dataType': 'html',
+        'url': js,
+        'data': { 'request': "", 'target': 'arrange_url', 'method': 'method_target' },
+        'success': function (data) {
+            tmp = 1;
+        }
+    });
+    return tmp;
+}();
+if(c==0)
+{
+    cards[i]=cards[i]+"%20%28Trading%20Card%29";
+}
 
-        console.log(cards[i]);
+
+console.log(cards[i]);
     }
-   var gid2=window.location.pathname.split("/");
    var url="https://steamcommunity.com/market/multibuy?appid=753";
-   var gid=gid2[4];
    var url2="";
     for(i=0;i<cards.length;i=i+2)
     {
         url2=url2+"&items[]="+gid+"-"+cards[i];
     }
     var url3=url+url2
-    window.open(url3);
+    console.log(url3);
 
 
-    })
+
+
+    });
